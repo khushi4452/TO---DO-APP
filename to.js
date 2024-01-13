7806c5a34acce4e1940625b30bd5d163eb712846
KHUSHI 444 <147485713+khushi4452@users.noreply.github.com>
2024-01-13 16:28:38 +0000

    added for of function

--- to.js
+++ to.js
@@ -1,10 +1,19 @@
-const todoList =["Go to the Gym","Learn Javascript ","complete the Assignment","Attend the ICP class"];
+const todoList = [
+   "Go to the gym",
+   "Learn javascript",
+   "complete the assignment",
+   "Attend the ICP class",
+"Go the sleep by 10:30",
+];
+
 
 const listcontainer = document.getElementById("list-container");
-for (const task of todoList){
-   listcontainer.innerHTML =  listcontainer.innerHTML + ' <div class="list-item">$(task)</div>';
-   
+
+for (const task of todoList ) {
+   listcontainer.innerHTML = listcontainer.innerHTML + '<div class="list-item">${task}</div>';
+
 }
+
 
 
 
