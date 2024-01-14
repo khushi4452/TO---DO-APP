e50c8f06f63759a7b4920717a241741b7b119b32
KHUSHI 444 <147485713+khushi4452@users.noreply.github.com>
2024-01-14 10:43:58 +0000

    added scroll to the css

--- to.js
+++ to.js
@@ -1,18 +1,43 @@
-const todoList = [
-   "Go to the gym",
-   "Learn javascript",
-   "complete the assignment",
-   "Attend the ICP class",
-"Go the sleep by 10:30",
-];
+const todoList = [];
 
 
 const listcontainer = document.getElementById("list-container");
 
 for (const task of todoList ) {
-   listcontainer.innerHTML = listcontainer.innerHTML + '<div class="list-item">${task}</div>';
+   listcontainer.innerHTML = 
+   listcontainer.innerHTML + '<div class="list-item">${task}</div>';
 
 }
+
+function addTask() {
+   const inputBox = document.getElementById("todo-input");
+   const task = inputBox.value ;
+   if(task){
+      todoList.push(task);
+      inputBox.value = "";
+      loadList();
+   }
+   else {
+      alert("please enter a task");
+   }
+}
+
+function loadList(){
+   listcontainer.innerHTML = "";
+
+   for (let i = 0; 1 < todoList.length; i++ ) {
+      const task = todoList (i);
+      listcontainer.innerHTML = 
+      listcontainer.innerHTML + '<div class="list-item"> ${task}  <span onclick ="RemoveTask(${i})"></span></div>';
+   
+}
+  }
+
+  function removeTask(index) {
+   todoList.slice(index,1);
+   loadList();
+  }
+
 
 
 
